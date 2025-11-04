98623e0bc81c86780f49c4e0474a143af3aa69c7
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-04 20:09:55 +0600

    not found file add

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div>
+            The requested resource was not found!
+        </div>
+    );
+};
+
+export default notFound;
